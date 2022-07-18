62f0a885ab2fdeccdb0d8ad5cd689d8e3e78a9ed
Fred Lindsey <fred.lindsey.92@gmail.com>
2022-07-18 16:03:27 -0500

    finalized acquire and prepare, started the parse modeule

--- acquire.py
+++ acquire.py
@@ -7,15 +7,17 @@ import pandas as pd
 
 #'data-science/jobs-after-a-coding-bootcamp-part-1-data-science/',
 
-url = 'https://codeup.com/'
 
-headers = {'User-Agent': 'CodeUp Data Science Student2'} #make sure this is rock solid, otherwise you can get a 403 error
 
-endpoints = ['data-science/jobs-after-a-coding-bootcamp-part-1-data-science/','featured/what-jobs-can-you-get-after-a-coding-bootcamp-part-2-cloud-administration/',
-'tips-for-prospective-students/is-our-cloud-administration-program-right-for-you/', 'codeup-news/inclusion-at-codeup-during-pride-month-and-always/', 
-'tips-for-prospective-students/mental-health-first-aid-training/']
+def title_and_content_scrapes(use_cache=True):
+    url = 'https://codeup.com/'
 
-def title_and_content_scrapes(endpoints, use_cache=True):
+    headers = {'User-Agent': 'CodeUp Data Science Student2'} #make sure this is rock solid, otherwise you can get a 403 error
+
+    endpoints = ['data-science/jobs-after-a-coding-bootcamp-part-1-data-science/','featured/what-jobs-can-you-get-after-a-coding-bootcamp-part-2-cloud-administration/',
+    'tips-for-prospective-students/is-our-cloud-administration-program-right-for-you/', 'codeup-news/inclusion-at-codeup-during-pride-month-and-always/', 
+    'tips-for-prospective-students/mental-health-first-aid-training/']
+
     filename = "title_and_content.csv"
     if os.path.isfile(filename) and use_cache:
         return pd.read_csv(filename)
@@ -55,10 +57,9 @@ def get_news_articles(use_cache=True):
         #breakers: if all news is removed as the first item, and if india is changed to something else
         categories[0] = 'national'
         # set up an empty list, to house the dictionaries
-
+        inshorts = []
         #loop through the categores
         for category in categories:
-            inshorts = []
             url = 'https://inshorts.com/en/read/' + category
             headers = {'User-Agent': 'CodeUp Data Science Student2'}
             response = get(url, headers=headers)
